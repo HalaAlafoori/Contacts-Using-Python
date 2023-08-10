2ef46db2b7e53039b61e95cd1436833ceb53d322
Hala Alafoori <moha6.afoori@gmail.com>
2023-08-10 22:09:36 +0300

    class construction & add function

--- contacts project.py
+++ contacts project.py
@@ -1,10 +1,29 @@
-
+class Contact:
+    def __init__(self, name, email, *phone_numbers):
+        self.name = name
+       
+        self.email = email
+        self.phone_numbers = phone_numbers #a list 
 
 
 
 
 def add():
-    print("add")
+    name=input("Enter name: \n")
+    email=input("Enter email: \n")
+    phone_numbers=[]
+    
+    while True:
+        phone_number=input("Enter phone number: \n")
+        phone_numbers.append(phone_number)
+        answer=input("Is there another phone number? (Y/N) \n")
+        if answer == 'N' or answer=='n':
+            break
+        
+    all_contacts.append(Contact(name, email, phone_numbers))
+    
+    
+    print(all_contacts[0].name)
     
 def edit():
     print("edit")
@@ -23,7 +42,10 @@ def search():
 
 
 run=True
+#creating the list of objects 
+all_contacts=[] #empty at first
 while run:
+   
     choice=input(
 "To add a new contact enter 1 \n\
 To edit a contact enter 2\n\
@@ -33,5 +55,5 @@ To search enter 5\n\
 To exit enter 6\n\
 ")
                  
-    choice_function={'1':add, 2:edit, 3:delete, 4:show, 5:search}
+    choice_function={'1':add, '2':edit, '3':delete, '4':show, '5':search}
     choice_function[choice]()
